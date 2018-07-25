93b60256afb56c701ad449f418f8d3fbfff60286
Toni Kusnandar <kusnandartoni@gmail.com>
2018-07-25 17:47:46 +0700

    add fabric theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Fabric>
+          <DefaultButton>
+            I am a button.
+          </DefaultButton>
+        </Fabric>
       </div>
     );
   }
